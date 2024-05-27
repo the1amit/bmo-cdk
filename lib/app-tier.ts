bbf4adace3120c8b938c3770faec7cebcbb8c81b
amit tailor <the_amit@live.com>
2024-05-27 19:53:51 -0400

    Application Tier

---
+++ lib/app-tier.ts
@@ -0,0 +1,55 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import { Construct } from 'constructs';
+
+interface AppTierProps extends cdk.StackProps {
+  vpc: ec2.Vpc;
+  dbCluster: rds.DatabaseCluster;
+}
+
+export class AppTier extends Construct {
+  public readonly service: ecs.FargateService;
+
+  constructor(scope: Construct, id: string, props: AppTierProps) {
+    super(scope, id);
+
+    // Create ECS cluster
+    const cluster = new ecs.Cluster(this, 'EcsCluster', {
+      vpc: props.vpc,
+    });
+
+    // Define Docker image asset if  Docker image is not publicly available
+    //const imageAsset = new ecr_assets.DockerImageAsset(this, 'WooCommerceImage', {
+      //directory: 'path/to/woocommerce/dockerfile',  // Update with the correct path
+    //});
+
+    // Define Fargate task definition
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
+    const container = taskDefinition.addContainer('WooCommerceContainer', {
+      image: ecs.ContainerImage.fromRegistry('woocommerce/woocommerce'),
+     // image: ecs.ContainerImage.fromDockerImageAsset(imageAsset), // If using local Docker Image path
+      memoryLimitMiB: 512,
+      cpu: 256,
+      environment: {
+        DB_HOST: props.dbCluster.clusterEndpoint.hostname,
+        DB_NAME: 'woocommerce',
+        DB_USER: 'admin',
+        DB_PASSWORD: 'adminpassword', // Use secrets manager for real applications
+      },
+    });
+
+    container.addPortMappings({
+      containerPort: 80,
+    });
+
+    // Create Fargate service
+    this.service = new ecs.FargateService(this, 'FargateService', {
+      cluster,
+      taskDefinition,
+      desiredCount: 2,
+    });
+  }
+}
--- lib/bmo-cdk-stack.ts
+++ lib/bmo-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { DbTier } from './db-tier';
+import { AppTier } from './app-tier';
 
 export class BmoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,6 +9,7 @@ export class BmoCdkStack extends cdk.Stack {
 
     // Instantiate each tier
     const dbTier = new DbTier(this, 'DbTier');
+    const appTier = new AppTier(this, 'AppTier', { vpc: dbTier.vpc, dbCluster: dbTier.dbCluster });
 
   }
 }
