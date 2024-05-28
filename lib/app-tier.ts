a9d1ea1a7d266748bf84db9d54b2e9fc2a3a5048
amit tailor <the_amit@live.com>
2024-05-27 21:13:14 -0400

    Updated docker image

--- lib/app-tier.ts
+++ lib/app-tier.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
+//import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import { Construct } from 'constructs';
 
@@ -21,7 +21,7 @@ export class AppTier extends Construct {
       vpc: props.vpc,
     });
 
-    // Define Docker image asset if  Docker image is not publicly available
+    // Define Docker image asset if using local Docker image
     //const imageAsset = new ecr_assets.DockerImageAsset(this, 'WooCommerceImage', {
       //directory: 'path/to/woocommerce/dockerfile',  // Update with the correct path
     //});
@@ -29,8 +29,8 @@ export class AppTier extends Construct {
     // Define Fargate task definition
     const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef');
     const container = taskDefinition.addContainer('WooCommerceContainer', {
-      image: ecs.ContainerImage.fromRegistry('woocommerce/woocommerce'),
-     // image: ecs.ContainerImage.fromDockerImageAsset(imageAsset), // If using local Docker Image path
+      image: ecs.ContainerImage.fromRegistry('woocommerce/woocommerce'), // Update with the correct public image name from Docker Hub
+     // image: ecs.ContainerImage.fromDockerImageAsset(imageAsset), // Uncomment this if using a local Docker image.
       memoryLimitMiB: 512,
       cpu: 256,
       environment: {
